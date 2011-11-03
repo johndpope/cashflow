74436580a760277cb6c25b72f909fed4fa247a57
Takuya Murakami <tmurakam@tmurakam.org>
2011-11-03 21:39:33 +0900

    add logic tests

---
+++ LogicTests/LogicTests.m
@@ -0,0 +1,32 @@
+//
+//  LogicTests.m
+//  LogicTests
+//
+//  Created by 卓弥 村上 on 11/11/03.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "LogicTests.h"
+
+@implementation LogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LogicTests");
+}
+
+@end
