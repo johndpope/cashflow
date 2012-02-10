0fe719bd21fb639f76c2cad1a306cc9498cced21
Takuya Murakami <tmurakam@tmurakam.org>
2012-02-10 18:51:55 +0900

    update BackupServer to use SQL

--- Classes/BackupServer.m
+++ Classes/BackupServer.m
@@ -14,6 +14,8 @@
 
 @implementation BackupServer
 
+#define BACKUP_NAME @"CashflowBackup.sql"
+
 - (void)requestHandler:(int)s filereq:(NSString*)filereq body:(char *)body bodylen:(int)bodylen
 {
     // Request to '/' url.
@@ -23,7 +25,7 @@
     }
 
     // download
-    else if ([filereq hasPrefix:@"/" DBNAME]) {
+    else if ([filereq hasPrefix:@"/" BACKUP_NAME]) {
         [self sendBackup:s];
     }
             
@@ -42,7 +44,7 @@
 
     [self send:s string:@"<html><body>"];
     [self send:s string:@"<h1>Backup</h1>"];
-    [self send:s string:@"<form method=\"get\" action=\"/" DBNAME "\"><input type=submit value=\"Backup\"></form>"];
+    [self send:s string:@"<form method=\"get\" action=\"/" BACKUP_NAME "\"><input type=submit value=\"Backup\"></form>"];
 
     [self send:s string:@"<h1>Restore</h1>"];
     [self send:s string:@"<form method=\"post\" enctype=\"multipart/form-data\"action=\"/restore\">"];
@@ -57,7 +59,14 @@
 */
 - (void)sendBackup:(int)s
 {
-    NSString *path = [[Database instance] dbPath:DBNAME];
+    DataModel *m = [DataModel instance];
+    NSString *path = [m getBackupSqlPath];
+
+    if (![m backupDatabaseToSql:path]) {
+        // write local file error...
+        // TBD
+        return;
+    }
 
     int f = open([path UTF8String], O_RDONLY);
     if (f < 0) {
@@ -106,15 +115,10 @@
     }
     if (!end) return;
 
-    // Check data format
-    if (strncmp(start, "SQLite format 3", 15) != 0) {
-        [self send:s string:@"HTTP/1.0 200 OK\r\nContent-Type:text/html\r\n\r\n"];
-        [self send:s string:@"This is not cashflow database file. Try again."];
-        return;
-    }
+    // Save data between start and end.
+    DataModel *m = [DataModel instance];
+    NSString *path = [m getBackupSqlPath];
 
-    // okay, save data between start and end.
-    NSString *path = [[Database instance] dbPath:DBNAME];
     int f = open([path UTF8String], O_WRONLY);
     if (f < 0) {
         // TBD;
@@ -127,6 +131,13 @@
         p += len;
     }
     close(f);
+
+    // restore
+    if (![m restoreDatabaseFromSql:path]) {
+        [self send:s string:@"HTTP/1.0 200 OK\r\nContent-Type:text/html\r\n\r\n"];
+        [self send:s string:@"This is not cashflow backup file. Try again."];
+        return;
+    }
 
     // send reply
     [self send:s string:@"HTTP/1.0 200 OK\r\nContent-Type:text/html\r\n\r\n"];
