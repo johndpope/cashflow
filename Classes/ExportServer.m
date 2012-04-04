d7cc9d17093d8672eee63cf8225a9da6d8d19006
Takuya Murakami <tmurakam@tmurakam.org>
2012-04-04 18:20:45 +0900

    fix license notice

--- Classes/ExportServer.m
+++ Classes/ExportServer.m
@@ -1,7 +1,7 @@
 // -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
 /*
  * CashFlow for iOS
- * Copyright (C) 2008-2011, Takuya Murakami, All rights reserved.
+ * Copyright (C) 2008-2012, Takuya Murakami, All rights reserved.
  * For conditions of distribution and use, see LICENSE file.
  */
 
