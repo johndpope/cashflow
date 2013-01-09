6ea4c49b0178e8b47b77975a8419a629159defbe
Takuya Murakami <tmurakam@tmurakam.org>
2013-01-10 00:23:06 +0900

    add ORQuery

---
+++ Classes/ormapper/ORQuery.h
@@ -0,0 +1,27 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+
+#import <UIKit/UIKit.h>
+#import "Database.h"
+
+/**
+   O/R query
+ */
+@interface ORQuery : NSObject
+{
+}
+
++ (ORQuery *)getWithClass:(Class)class tableName:(NSString *)tableName;
+
+- (id)initWithClass:(Class)class tableName:(NSString *)tableName;
+
+- (ORQuery *)where:(NSString *)where arguments:(NSArray *)args;
+- (ORQuery *)order:(NSString *)order;
+- (ORQuery *)limit:(int)limit;
+- (ORQuery *)offset:(int)limit;
+
+- (NSMutableArray *)all;
+- (id)first;
+
+@end
+
+
