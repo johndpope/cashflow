6cdd10c86903be5f39d7c86a588ed0e700f1ad1d
Takuya Murakami <tmurakam@tmurakam.org>
2013-09-28 22:14:06 +0900

    bug fix

--- Classes/controllers/ReportVC.m
+++ Classes/controllers/ReportVC.m
@@ -1,4 +1,4 @@
-1;2c// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
 /*
  * CashFlow for iOS
  * Copyright (C) 2008-2011, Takuya Murakami, All rights reserved.
