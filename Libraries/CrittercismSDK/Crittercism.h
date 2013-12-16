4fa75050feb166e2750e7233870cd2766f9b866d
Takuya Murakami <tmurakam@tmurakam.org>
2013-12-16 22:32:21 +0900

    update Crittercism SDK 4.0.7 -> 4.3.0

--- Libraries/CrittercismSDK/Crittercism.h
+++ Libraries/CrittercismSDK/Crittercism.h
@@ -10,10 +10,7 @@
 
 // Operating System Support
 //
-// The Crittercism iOS library supports iOS v4.3+
-//
-// The OPTMZ network instrumentation component will only be enabled on iOS v5.0
-// and higher.
+// The Crittercism iOS library supports iOS v5.0+
 
 // Additional Requirements:
 //
@@ -28,6 +25,8 @@
 // Example:
 //
 // [Crittercism enableWithAppID:@"YOURAPPIDGOESHERE"];
+
+@class CLLocation;
 
 @interface Crittercism : NSObject
 
@@ -97,6 +96,10 @@
 // instruct the library to perform all breadcrumb writes on a background thread.
 
 + (void)setAsyncBreadcrumbMode:(BOOL)writeAsync;
+
+// Inform Crittercism of the device's most recent location for use with
+// performance monitoring.
++ (void)updateLocation:(CLLocation *)location;
 
 // Handled exceptions are a way of reporting exceptions your app intentionally
 // caught. If the passed in NSException object was @thrown, the stack trace
