4ee23ee4ad03e44b0205d8ab19c6f3c128d2dd07
Takuya Murakami <tmurakam@tmurakam.org>
2013-12-28 23:52:39 +0900

    remove deprecated api call

--- Classes/controllers/BackupVC.m
+++ Classes/controllers/BackupVC.m
@@ -44,7 +44,7 @@
 
 - (IBAction)doneAction:(id)sender
 {
-    [self.navigationController dismissModalViewControllerAnimated:YES];
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
     [_delegate backupViewFinished:self];
 }
 
