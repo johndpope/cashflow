ebd5ebdad0e921e037bcb7fe2788cb4f452ce087
Takuya Murakami <tmurakam@tmurakam.org>
2014-09-23 17:54:47 +0900

    Add InfoViewController.swift, bridging header

---
+++ Classes/controllers/InfoViewController.swift
@@ -0,0 +1,12 @@
+//
+// InfoViewController.swift
+//
+
+import UIKit
+
+class InfoViewController : UIViewController {
+    @IBOutlet var _nameLabel : UILabel!
+    @IBOutlet var _versionLabel: UILabel!
+    @IBOutlet var _purchaseButton: UIButton!
+
+}
